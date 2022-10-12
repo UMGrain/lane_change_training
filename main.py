6e012a7eafa7ef8b2ad73871139a292275eaa598
谷溢 <guyi@guyideMacBook-Air.local>
2022-10-12 16:27:45 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import pathlib
+import torch
+
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+from typing import Tuple, Dict, List
+
+if __name__ == '__main__':
+    train_dir = "/mnt/disk02/carla_lb2/lane_change_data/ConstructionObstacle/"
+
+    # Setup path for target directory
+    target_directory = train_dir
+    print(f"Target directory: {target_directory}")
+
+    # Get the class names from the target directory
+    class_names_found = sorted([entry.name for entry in list(os.scandir(image_path / "train"))])
+    print(f"Class names found: {class_names_found}")
+
+
